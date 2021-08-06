fc8b26fa4fde290f8403a056172203c1f28d046b
Arman Kheirandish <arman.kheirandish@gmail.com>
2021-08-06 14:26:08 +0430

    fix: remove white background on En flag

--- assets/images/icons/english.vue
+++ assets/images/icons/english.vue
@@ -3,10 +3,9 @@
     id="Layer_1"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
+    x="32"
+    y="32"
     viewBox="0 0 32 32"
-    xml:space="preserve"
   >
     <style>
       .st1 {
@@ -16,63 +15,42 @@
         fill: #092c70;
       }
     </style>
-    <g id="Layer_2_1_">
-      <g id="g3923" transform="matrix(2.8449 0 0 2.9076 -1304.9 -535.8)">
-        <path fill="#fff" d="M458.7 184.3h11.2v11h-11.2z" />
-        <path class="st1" d="M469.9 189.1h-5v-3h-1.2v3h-5v1.3h5v3h1.2v-3h5z" />
-        <g id="g3025" transform="translate(61 -105)">
-          <path id="path3013" class="st2" d="M398.9 291.1l3.6 2.6v-2.6h-3.6" />
-          <path id="path3017" class="st2" d="M397.7 291.7v2.2h3.2l-3.2-2.2" />
-          <path
-            id="path3021"
-            class="st1"
-            d="M397.7 291.1v.3l3.7 2.5h.5l-4.2-2.8"
-          />
-        </g>
-        <g id="g3025-0" transform="matrix(-1 0 0 1 1120 -105)">
-          <path
-            id="path3013-9"
-            class="st2"
-            d="M651.3 291.1l3.6 2.6v-2.6h-3.6"
-          />
-          <path id="path3017-4" class="st2" d="M650.1 291.7v2.2h3.2l-3.2-2.2" />
-          <path
-            id="path3021-8"
-            class="st1"
-            d="M650.1 291.1v.3l3.7 2.5h.5l-4.2-2.8"
-          />
-        </g>
-        <g id="g3025-5" transform="rotate(180 560 419.9)">
-          <path
-            id="path3013-1"
-            class="st2"
-            d="M651.3 646.3l3.6 2.6v-2.6h-3.6"
-          />
-          <path id="path3017-7" class="st2" d="M650.1 646.9v2.2h3.2l-3.2-2.2" />
-          <path
-            id="path3021-1"
-            class="st1"
-            d="M650.1 646.3v.3l3.7 2.5h.5l-4.2-2.8"
-          />
-        </g>
-        <g id="g3025-0-7" transform="matrix(-1 0 0 1 1058 -62)">
-          <path
-            id="path3013-9-6"
-            class="st2"
-            d="M595.7 252.6l3.6 2.6v-2.6h-3.6"
-          />
-          <path
-            id="path3017-4-1"
-            class="st2"
-            d="M594.5 253.2v2.2h3.2l-3.2-2.2"
-          />
-          <path
-            id="path3021-8-4"
-            class="st1"
-            d="M594.5 252.6v.3l3.7 2.5h.5l-4.2-2.8"
-          />
-        </g>
-      </g>
+    <path d="M0 6h32v20H0V6z" fill="#fff" />
+    <path
+      class="st1"
+      d="M32 14.2H17.7V6h-3.4v8.2H0v3.6h14.3V26h3.4v-8.2H32v-3.6z"
+    />
+    <g id="g3025" transform="translate(61 -105)">
+      <path id="path3013" class="st2" d="M-57.6 111l10.3 7.1V111h-10.3" />
+      <path id="path3017" class="st2" d="M-61 112.6v6h9.1l-9.1-6" />
+      <path id="path3021" class="st1" d="M-61 111v.8l10.6 6.8h1.4l-12-7.6" />
+    </g>
+    <g id="g3025-0" transform="matrix(-1 0 0 1 1120 -105)">
+      <path id="path3013-9" class="st2" d="M1091.4 111l10.3 7.1V111h-10.3" />
+      <path id="path3017-4" class="st2" d="M1088 112.6v6h9.1l-9.1-6" />
+      <path id="path3021-8" class="st1" d="M1088 111v.8l10.6 6.8h1.4l-12-7.6" />
+    </g>
+    <g id="g3025-5" transform="rotate(180 560 419.9)">
+      <path id="path3013-1" class="st2" d="M1091.4 813.8l10.3 7.1v-7.1h-10.3" />
+      <path id="path3017-7" class="st2" d="M1088 815.4v6h9.1l-9.1-6" />
+      <path
+        id="path3021-1"
+        class="st1"
+        d="M1088 813.8v.8l10.6 6.8h1.4l-12-7.6"
+      />
+    </g>
+    <g id="g3025-0-7" transform="matrix(-1 0 0 1 1058 -62)">
+      <path
+        id="path3013-9-6"
+        class="st2"
+        d="M1047.7 80.3l10.3 7.1v-7.1h-10.3"
+      />
+      <path id="path3017-4-1" class="st2" d="M1044.3 82v6h9.1l-9.1-6" />
+      <path
+        id="path3021-8-4"
+        class="st1"
+        d="M1044.3 80.3v.8l10.6 6.8h1.4l-12-7.6"
+      />
     </g>
   </svg>
 </template>
