91a17fb24e3f26e8e7912b85c54a910f163fd028
Amin <sma.azar@gmail.com>
2021-09-11 07:43:57 +0430

    Final firebase touches

--- firestore-service.js
+++ firestore-service.js
@@ -3,19 +3,18 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCgEznAgq9aklfAMC43jHu4riVuTXIXSJQ',
-  authDomain: 'test-project-3c1dd.firebaseapp.com',
-  databaseURL: 'https://test-project-3c1dd.firebaseio.com',
-  projectId: 'test-project-3c1dd',
-  storageBucket: 'test-project-3c1dd.appspot.com',
-  messagingSenderId: '986034417692',
-  appId: '1:986034417692:web:a403d92d5e06537753afd9',
-  measurementId: 'G-VZHJLWHX4B',
+  apiKey: 'AIzaSyCSAyjOjmBWtNaEUSaMbFTO70z4hbUlhUc',
+  authDomain: 'bos-website-84e11.firebaseapp.com',
+  projectId: 'bos-website-84e11',
+  storageBucket: 'bos-website-84e11.appspot.com',
+  messagingSenderId: '586449931051',
+  appId: '1:586449931051:web:629b1d6db8de23fa66d36e',
+  measurementId: 'G-47GHGXNEZV'
 }
 export const db = firebase.initializeApp(firebaseConfig).firestore()
 export const fileStorage = firebase.storage()
 export const TaskState = firebase.storage.TaskState
-// Export types that exists in Firestore
-// This is not always necessary, but it's used in other examples
+export const timestamped = obj => ({...obj, created: firebase.firestore.FieldValue.serverTimestamp()})
+
 const { Timestamp, GeoPoint } = firebase.firestore
 export { Timestamp, GeoPoint }
